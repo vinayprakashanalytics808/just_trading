9578698aa0488034a01b9543c7cdae57a9dddd7a
vinayprakash808 <vinayprakash808@gmail.com>
2024-03-28 18:14:34 +0530

    changed function

--- app.R
+++ app.R
@@ -16,28 +16,38 @@ library(rhandsontable)
 library(kableExtra)
 library(reactable)
 library(glue)
+library(shinyjs)
+library(shinycssloaders)
 source("DButils.R")
 source("functions.R")
 
 ui <- fluidPage(includeCSS("www/design.css"),includeScript("www/design.js"),
   br(),
   # uiOutput("getCom"),
-  fluidRow(
-            column(3,uiOutput("getDiff")),
-            column(3,uiOutput("getDHP")),
-            column(3,uiOutput("getEPS")),
-            column(3,uiOutput("getPrice"))),
-  fluidRow(
-    column(3,uiOutput("getID")),
-    column(3,uiOutput("get5D"))),
-  br(),
-  reactableOutput("details")
+  tabsetPanel(
+    type = "tabs",
+    tabPanel("Summary",   br(), fluidRow(
+        column(3,uiOutput("getDiff")),
+        column(3,uiOutput("getDHP")),
+        column(3,uiOutput("getEPS")),
+        column(3,uiOutput("getPrice")),
+      br()),
+      fluidRow(
+        column(3,uiOutput("getID")),
+        column(3,uiOutput("get5D"))),
+        br(),
+        reactableOutput("details")%>% withSpinner(color="#0dc5c1")
+      ),
+        tabPanel("Profit Prediction", br(), fluidRow(span(column(3,(div(style="margin-left:5%", numericInput("investment", "Investment :", value = 100000)))),
+                                                          column(3,div(numericInput("returns", "No of days to get return :", value = 1, max = 10, min = 1))))), br(),
+                 fluidRow(column(6, br(), br(), br(), div(plotlyOutput("prof_plot")%>% withSpinner(color="#0dc5c1"))),
+                          column(6, div(reactableOutput("prof_details")%>% withSpinner(color="#0dc5c1"))))))
 )
 
 server <- function(input, output, session) {
   
   storage <- reactiveValues()
-  # storage$data <- data.frame()
+  storage$modelData <- data.frame()
   # storage$company_info <- data.frame()
   # storage$real_Time_price <- data.frame()
   # storage$Stock_price_Change <- data.frame()
@@ -45,68 +55,88 @@ server <- function(input, output, session) {
   main_company_info <- do.call(rbind.data.frame, lapply(rjson::fromJSON(file = get_Links('To get Company Information', TRUE)), nullToNA))
   main_real_Time_price <- do.call(rbind.data.frame, lapply(rjson::fromJSON(file = get_Links('To get Real Time Price', TRUE)), nullToNA))
   main_Stock_price_Change <- do.call(rbind.data.frame, lapply(rjson::fromJSON(file = get_Links('To get Stock Price Change', TRUE)), nullToNA))
+  historical_Price <- lapply(rjson::fromJSON(file = gsub("2021-09-24", Sys.Date(), gsub("2021-08-24", Sys.Date()-20, get_Links('To get historical prices', TRUE)))), nullToNA) %>% pluck("historicalStockList") %>%  map_dfr(~ as_tibble(.x) %>% unnest_wider(historical))
   main_data['yearHigh - dayHigh'] <- round(main_data['yearHigh'],0) - round(main_data['dayHigh'], 0)
   main_data['eps'] <- round(main_data['eps'],2)
   
 
-  company_info_Columns <- c('description', 'industry', 'sector','isin')
+  company_info_Columns <- c('description', 'industry', 'sector', 'isin', 'isActivelyTrading', 'fullTimeEmployees')
   real_Time_price_Columns <- c('prevClose', 'low')
   Stock_price_Change_Columns <- c('X1D', 'X5D')
     
-    # for (i in company_info_Columns){
-    #   main_data[i] <- main_company_info[[i]][match(main_company_info[['symbol']],main_data[['symbol']])]
-    # }
-    # 
-    # for (i in real_Time_price_Columns){
-    #   main_data[i] <- main_real_Time_price[[i]][match(main_real_Time_price[['symbol']],main_data[['symbol']])]
-    # }
-    # 
-    # for (i in Stock_price_Change_Columns){
-    #   main_data[i] <- main_Stock_price_Change[[i]][match(main_Stock_price_Change[['symbol']],main_data[['symbol']])]
-    # }
-  
   main_data <- inner_join(main_data, main_company_info[c(company_info_Columns, 'symbol')], by='symbol')
   main_data <- inner_join(main_data, main_real_Time_price[c(real_Time_price_Columns, 'symbol')], by='symbol')
   main_data <- inner_join(main_data, main_Stock_price_Change[c(Stock_price_Change_Columns, 'symbol')], by='symbol')
 
-  required_Columns <- c('name', 'price', 'yearHigh - dayHigh', 'dayHigh', 'eps', 'marketCap', 'pe', company_info_Columns,real_Time_price_Columns,Stock_price_Change_Columns)
+  required_Columns <- c('symbol', 'name', 'price', 'yearHigh - dayHigh', 'dayHigh', 'eps', 'marketCap', 'pe', 'open', 'volume', 'priceAvg50', 'priceAvg200', company_info_Columns,real_Time_price_Columns,Stock_price_Change_Columns)
+  hidden_Columns <- c("symbol", "marketCap", "pe", "description", "industry", "sector", "isin", "prevClose", "low", "open", "pc", "X1D", "X5D", "volume", "isActivelyTrading", "priceAvg50", "priceAvg200") 
     
     
-    
-
-  print(colnames(main_data))
-
   output$details <- renderReactable({
   req(input$getDiff_id, input$getDHP_id)
       
   # browser()    
-  main_data <- main_data[required_Columns][order(main_data['yearHigh - dayHigh'], decreasing = FALSE),] %>% filter(between(`yearHigh - dayHigh`, input$getDiff_id[1], input$getDiff_id[2]),
+  main_data <- main_data[required_Columns][order(main_data['yearHigh - dayHigh'], decreasing = FALSE),] %>% mutate_at(c('fullTimeEmployees'),
+                                                                                                                      as.numeric) %>% filter(low > 0, 
+                                                                                                                   marketCap > 0, 
+                                                                                                                   volume > 0,
+                                                                                                                   isActivelyTrading == TRUE,
+                                                                                                                   between(`yearHigh - dayHigh`, input$getDiff_id[1], input$getDiff_id[2]),
                                                                                                                    between(dayHigh, input$getDHP_id[1], input$getDHP_id[2]),
                                                                                                                    between(X1D, input$getID_id[1], input$getID_id[2]),
                                                                                                                    between(X5D, input$get5D_id[1], input$get5D_id[2]))
   main_data$pc <- do.call(paste, c(main_data[Stock_price_Change_Columns], sep = '_'))
-  main_data[['name']] <- sapply(paste0(main_data[['name']],"_",main_data[['isin']],"_",main_data[['pc']]),function(x) htmltools::HTML(unlist(strsplit(x, "_"))[1],'<span class="a">', 
-                                                                                                'ISIN : ', unlist(strsplit(x, "_"))[2],'</span>&nbsp',
+  main_data[['name']] <- sapply(paste0(
+                                           main_data[['name']],
+                                       "_",main_data[['isin']],
+                                       "_",main_data[['pc']],
+                                       "_",main_data[['priceAvg50']],
+                                       "_",main_data[['priceAvg200']]),function(x) htmltools::HTML(unlist(strsplit(x, "_"))[1],
+                                                                                                '<span class="a">','ISIN : ', unlist(strsplit(x, "_"))[2],'</span>&nbsp',
                                                                                                 price_Change("1D", unlist(strsplit(x, "_"))[3]),
-                                                                                                price_Change("5D", unlist(strsplit(x, "_"))[4]))
+                                                                                                price_Change("5D", unlist(strsplit(x, "_"))[4]),
+                                                                                                '<sup style="font-size: smaller">','50D avg : ', round(as.numeric(unlist(strsplit(x, "_"))[5]),2),'</sup>&nbsp',
+                                                                                                '<sup style="font-size: smaller">',' /&nbsp&nbsp200D avg : ', round(as.numeric(unlist(strsplit(x, "_"))[6]),2),'</sup>&nbsp'
+                                                                                                )
       )
+  
+  
       
-      
-      
-  reactable(class = "rt", rownames = F, columns = list(name = colDef(html = TRUE,width = 1000), 
-        marketCap = colDef(show =F), 
-        pe = colDef(show =F), 
-        `description` = colDef(show =F),
-        industry = colDef(show =F), 
-        sector = colDef(show =F),
-        isin = colDef(show =F),
-        prevClose = colDef(show =F),
-        low = colDef(show =F),
-        open = colDef(show =F),
-        pc = colDef(show =F),
-        X1D = colDef(show =F),
-        X5D = colDef(show =F)
-        ),
+#   main_data$ht <- '<html>
+# <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+# <body>
+# <canvas id="myChart" style="width:100%;max-width:600px"></canvas>
+# 
+# <script>
+# const xValues = [50,60,70,80,90,100,110,120,130,140,150];
+# const yValues = [7,8,8,9,9,9,10,11,14,14,15];
+# 
+# new Chart("myChart", {
+#   type: "line",
+#   data: {
+#     labels: xValues,
+#     datasets: [{
+#       fill: false,
+#       lineTension: 0,
+#       backgroundColor: "rgba(0,0,255,1.0)",
+#       borderColor: "rgba(0,0,255,0.1)",
+#       data: yValues
+#     }]
+#   },
+#   options: {
+#     legend: {display: false},
+#     scales: {
+#       yAxes: [{ticks: {min: 6, max:16}}],
+#     }
+#   }
+# });
+# </script>
+# 
+# </body>
+# </html>'   
+  # storage$modelData <- main_data     
+  reactable(class = "rt", rownames = F, defaultPageSize = 15, columns = c(list(name = colDef(html = TRUE,width = 1000,
+                                                                       filterable = TRUE)),lapply(setNames(hidden_Columns, hidden_Columns), function(x){x = colDef(show =F)})),
         defaultColDef = colDef(show = T), main_data, details = function(index) {
         htmltools::div(tags$style("table {
   font-family: arial, sans-serif;
@@ -123,7 +153,8 @@ td, th {
 
 tr:nth-child(even) {
 }"),tags$table(tags$tr(
-                  tags$th('Sector')
+                  tags$th('Symbol')
+                 ,tags$th('Sector')
                  ,tags$th('Industry')
                  ,tags$th('Company Info')
                  ,tags$th('PE')
@@ -133,7 +164,8 @@ tr:nth-child(even) {
                  ,tags$th('open')
                  )
                  ,tags$tr(
-                  tags$td(main_data[index,][['sector']])
+                  tags$td(main_data[index,][['symbol']])
+                 ,tags$td(main_data[index,][['sector']])
                  ,tags$td(main_data[index,][['industry']])
                  ,tags$td(main_data[index,][['description']])
                  ,tags$td(main_data[index,][['pe']])
@@ -146,15 +178,33 @@ tr:nth-child(even) {
       })
 })    
 
-
-  output$getDiff <- renderUI({div(class = "label-left",sliderInput("getDiff_id", "Difference", min = min(main_data['yearHigh - dayHigh']), max = max(main_data['yearHigh - dayHigh']), value = c(min(main_data['yearHigh - dayHigh']),max(main_data['yearHigh - dayHigh']))))})
-  output$getDHP <- renderUI({div(class = "label-left",sliderInput("getDHP_id", "Day High Price", min = round_of_two(min(main_data['dayHigh'])), max = round_of_two(max(main_data['dayHigh'])), value = c(min(main_data['dayHigh']),max(main_data['dayHigh']))))})
-  output$getEPS <- renderUI({div(class = "label-left",sliderInput("getEPS_id", "EPS", min = round_of_two(min(main_data['eps'],na.rm = TRUE)), max = round_of_two(max(main_data['eps'],na.rm = TRUE)), value = min(main_data['eps'],na.rm = TRUE)))})
-  output$getPrice <- renderUI({div(class = "label-left",sliderInput("getPrice_id", "Price", min = round_of_two(min(main_data['price'],na.rm = TRUE)), max = round_of_two(max(main_data['price'],na.rm = TRUE)), value = min(main_data['price'],na.rm = TRUE)))})
-  output$getID <- renderUI({div(class = "label-left",sliderInput("getID_id", "1D Change", min = min(main_data['X1D']), max = max(main_data['X1D']), value = c(min(main_data['X1D']),max(main_data['X1D']))))})
-  output$get5D <- renderUI({div(class = "label-left",sliderInput("get5D_id", "5D Change", min = min(main_data['X5D']), max = max(main_data['X5D']), value = c(min(main_data['X5D']),max(main_data['X5D']))))})
+  
+  output$getDiff <- renderUI({div(class = "label-left",sliderInput("getDiff_id", "Difference", min = min_num(main_data['yearHigh - dayHigh']), max = max_num(main_data['yearHigh - dayHigh']), value = c(min_num(main_data['yearHigh - dayHigh']),max_num(main_data['yearHigh - dayHigh']))))})
+  output$getDHP <- renderUI({div(class = "label-left",sliderInput("getDHP_id", "Day High Price", min = round_of_two(min_num(main_data['dayHigh'])), max = round_of_two(max_num(main_data['dayHigh'])), value = c(min_num(main_data['dayHigh']),max_num(main_data['dayHigh']))))})
+  output$getEPS <- renderUI({div(class = "label-left",sliderInput("getEPS_id", "EPS", min = round_of_two(min_num(main_data['eps'])), max = round_of_two(max_num(main_data['eps'])), value = min_num(main_data['eps'])))})
+  output$getPrice <- renderUI({div(class = "label-left",sliderInput("getPrice_id", "Price", min = round_of_two(min_num(main_data['price'])), max = round_of_two(max_num(main_data['price'])), value = min_num(main_data['price'])))})
+  output$getID <- renderUI({div(class = "label-left",sliderInput("getID_id", "1D Change", min = min_num(main_data['X1D']), max = max_num(main_data['X1D']), value = c(min_num(main_data['X1D']),max_num(main_data['X1D']))))})
+  output$get5D <- renderUI({div(class = "label-left",sliderInput("get5D_id", "5D Change", min = min_num(main_data['X5D']), max = max_num(main_data['X5D']), value = c(min_num(main_data['X5D']),max_num(main_data['X5D']))))})
+  
+  
+  main_data_selected <- lapply(rjson::fromJSON(file = gsub("2021-09-24", Sys.Date(), gsub("2021-08-24", Sys.Date()-60, gsub("companies_to_be_passed", capture.output(cat(as.character(main_data[which(main_data$`yearHigh - dayHigh` == 0),]$symbol), sep = ",")), get_Links('To get historical prices', FALSE))))), nullToNA) %>% pluck("historicalStockList") %>%  map_dfr(~ as_tibble(.x) %>% unnest_wider(historical))
+  # browser()
+  if(nrow(main_data_selected) > 1){
+    output$prof_plot <- renderPlotly({
+      profit_function_plot(main_data_selected, symbol, open, input$returns, input$investment)
+    })
+    
+    output$prof_details <- renderReactable({ 
+      reactable(class = "rt", rownames = F, as.data.frame(profit_function(main_data_selected, symbol, open, input$returns, input$investment))[c(1:6,15)],
+                columns = c(list(symbol = colDef(html = TRUE,
+                                               filterable = TRUE))))
+    })
+  }
+  
 
 }
+
+
 
 
 shinyApp(ui, server)
--- functions.R
+++ functions.R
@@ -9,6 +9,14 @@ round_of_two <- function(a){
   return(round(a,2))
 }
 
+min_num <- function(a){
+  return(round(min(a,na.rm = TRUE),2))
+}
+
+max_num <- function(a){
+  return(round(max(a,na.rm = TRUE),2))
+}
+
 
 add_collapse_content <- function(x, id) {
   tagList(
@@ -29,10 +37,10 @@ add_collapse_content <- function(x, id) {
 price_Change <- function(title, price){
   if (price > 0) 
   {
-    return(paste0('<span class="b">',title,' : ',round(as.numeric(price),2),'&nbsp<i></i></span>&nbsp'))
+    return(paste0('<span class="b" style="font-size: smaller">',title,' : ',round(as.numeric(price),2),'&nbsp<i></i></span>&nbsp'))
   } else 
   {
-    return(paste0('<span class="c">',title,' : ',round(as.numeric(price),2),'&nbsp<i></i></span>&nbsp'))
+    return(paste0('<span class="c" style="font-size: smaller">',title,' : ',round(as.numeric(price),2),'&nbsp<i></i></span>&nbsp'))
   }
 }
 
@@ -43,4 +51,34 @@ paste_function <- function(df, df_col = c(a, b)){
     return(substr(paste0(df,'$',df_col,collapse = ",", sep = ",'_'"), 1,nchar(paste0(df,'$',df_col,collapse = ",", sep = ",'_'"))-4))
   }
 }
+
+
+
+lag_function <- function(dataframe_to_call, category, open_price, number_of_days){
+  ind = number_of_days:98
+  category <- enquo(category)
+  open_price <- enquo(open_price)
+  varname <- paste("Day+", number_of_days , "_open", sep="")
+  return(dataframe_to_call %>% group_by({{category}}) %>% mutate({{varname}} := c({{open_price}}[head(ind+1, n())], rep(NA, max(0, n() - length(ind)-1)))))
+}
+
+profit_function <- function(dataframe_to_call, category, open_price, number_of_days, profit){
+  ind = number_of_days:98
+  category <- enquo(category)
+  open_price <- enquo(open_price)
+  varname <- paste("profit for ", profit, " in ", format(number_of_days, scientific = FALSE), " days", sep = "")
+  return(dataframe_to_call %>% group_by({{category}}) %>% mutate({{varname}} := round((profit * (close - c({{open_price}}[head(ind+1, n())], rep(NA, max(0, n() - length(ind)-1))))) / c({{open_price}}[head(ind+1, n())], rep(NA, max(0, n() - length(ind)-1))),0)))
+}
+
+back_ground = 'antiquewhite'
+profit_function_plot <- function(dataframe_to_call, category, open_price, number_of_days, profit){
+       ind = number_of_days:98
+       category <- enquo(category)
+       open_price <- enquo(open_price)
+       varname <- paste("profit for ", profit, " in ", format(number_of_days, scientific = FALSE), " days", sep = "")
+       new_data <- dataframe_to_call %>% group_by({{category}}) %>% mutate({{varname}} := (profit * (close - c({{open_price}}[head(ind+1, n())], rep(NA, max(0, n() - length(ind)-1))))) / c({{open_price}}[head(ind+1, n())], rep(NA, max(0, n() - length(ind)-1))))
+       new_data <- new_data[complete.cases(new_data),]
+       new_data$date <- as.Date(new_data$date)
+       return(ggplotly(new_data %>% ggplot(aes(x=date,y=!!sym(names(.)[15])))+geom_hline(aes(yintercept=profit), linetype='dashed')+geom_line(aes(color=symbol))+theme(panel.background = element_rect(fill = back_ground),plot.background = element_rect(fill = back_ground),legend.background = element_rect(fill = back_ground))+ ggtitle(colnames(new_data)[15]) + theme(plot.title = element_text(color="black", size=12, face="bold.italic",hjust = 0.5), axis.title.x=element_blank(),axis.text.x=element_blank(),axis.ticks.x=element_blank(),axis.title.y=element_blank(),axis.text.y=element_blank(),axis.ticks.y=element_blank())))
+   }
 
